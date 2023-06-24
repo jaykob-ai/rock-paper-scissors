63b49103e8d7dd0d5b8facf7d4959a0484ffc1b9
jaykob-ai <traveling.jakob@gmail.com>
2023-06-24 14:39:57 +0200

    do basic ui with three buttons and text messages

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let scorePlayer = 0;
 let scoreComputer = 0;
 let roundMessage;
 let playerSelection;
+let gameGoing = true;
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -15,36 +16,54 @@ function getComputerChoice() {
     return computerSelection;
 }
 
-function playRound() {
-    playerSelection = prompt("What's your move?", '');
-    playerSelection = playerSelection.toLowerCase();
+function playRound(playerSelection) {
+  if (gameGoing === true) {
     getComputerChoice();
 
     if (playerSelection === 'rock' & computerSelection === 'paper' ||
         playerSelection === 'paper' & computerSelection === 'scissors' ||
         playerSelection === 'scissors' & computerSelection === 'rock') {
       ++scoreComputer;
-      roundMessage = 'You lost this Round! ' + computerSelection + ' beats ' + playerSelection;
+    roundMessage = 'You lost this Round! ' + computerSelection + ' beats ' + playerSelection;
     } else if (playerSelection === 'rock' & computerSelection === 'scissors' ||
-               playerSelection === 'paper' & computerSelection === 'rock' ||
-               playerSelection === 'scissors' & computerSelection === 'paper') {
+                playerSelection === 'paper' & computerSelection === 'rock' ||
+                playerSelection === 'scissors' & computerSelection === 'paper') {
       ++scorePlayer;
       roundMessage = 'You won this Round! ' + playerSelection + ' beats ' + computerSelection;
-    } else if (playerSelection === computerSelection) {
-      roundMessage = 'This round is a draw!'
     } else {
-      roundMessage = `You have to type "rock", "paper" or "scissors"! "Well" doesn't count!`;
+      roundMessage = 'This round is a draw!';
     }
+    
+
+    const score = document.querySelector('.score');
+    score.textContent = 'Player: ' + scorePlayer + ', Computer: ' + scoreComputer;
+    const results = document.querySelector('.results');
+    results.textContent = roundMessage;
+
+    if (scorePlayer === 5) {
+      score.textContent = 'Player: ' + scorePlayer + ', Computer: ' + scoreComputer + '. Game is over! You won!';
+      return gameGoing = false;
+    } else if (scoreComputer === 5) {
+      score.textContent = 'Player: ' + scorePlayer + ', Computer: ' + scoreComputer + '. Game is over! You lost!';
+      return gameGoing = false;
+    }
+  }  
 }
 
-function game() {
+/*function game() {
     
     for (let i = 0; i < 5; i++) {
         playRound();
-        console.log(roundMessage);
     }
     let score = 'The final score is: Player: ' + scorePlayer + ' and Computer: ' + scoreComputer;
 
     return score;
-}
-console.log(game());
+}*/
+
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+
+rock.addEventListener('click', () => playRound('rock'));
+paper.addEventListener('click', () => playRound('paper'));
+scissors.addEventListener('click', () => playRound('scissors'));
